2cd057f46c6a1501891dcc7087a6af357c8a4ce3
Ella Vankirk <ella.vankirk@gmail.com>
2020-10-28 22:17:27 -0500

    Everything seems to be working as intended. Added json

--- script.js
+++ script.js
@@ -4,50 +4,64 @@ window.addEventListener("load", function() {
 });
 
    function init() {
-   
-      let pilotInput = document.querySelector("input[name=pilotName]");
       let form = document.querySelector("form");
+      let pilotInput = document.querySelector("input[name=pilotName]");
       let copilotInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-      let cargoMassInput = document.querySelector("input[name=cargoMass]");  
-      let faultyItems = document.getElementById("faultyItems") 
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+      let fuelLevelInputValue = Number("fuelLevelInput.value");
+      // let cargoMassInputValue = Number("cargoMassInputValue");
+
+      let faultyItems = document.getElementById("faultyItems");
       let pilotStatus = document.getElementById("pilotStatus");
       let copilotStatus = document.getElementById("copilotStatus");
       let fuelStatus = document.getElementById("fuelStatus");
       let cargoStatus = document.getElementById('cargoStatus');
-      let launchStatus = document.getElementById("launchStatusCheck");
+      let launchStatus = document.getElementById("launchStatus");
 
       form.addEventListener("submit", function(event) {
+         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+            response.json().then( function(json){
+               console.log(json);
+            });
+         });
          if (pilotInput.value === "" ||  copilotInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value ==="") {
             alert("All fields are required");
                event.preventDefault();
-         } else if (isNaN(pilotInput.value) === false || isNaN(copilotInput.value) === false) {
+         } else if (isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) { 
             alert("Fuel Level and Cargo Mass must be numbers.");
                event.preventDefault();
          }
-         
+        
+         pilotStatus.innerHTML = `Pilot ${pilotInput.value} Ready`
+         copilotStatus.innerHTML = `Co-pilot ${copilotInput.value} Ready`
+
          if (fuelLevelInput.value < 10000) {
             faultyItems.style.visibility = "visible";
             fuelStatus.innerHTML = `Not enough fuel.`;
             launchStatus.style.color = "red";
             launchStatus.innerHTML = (`Shuttle not ready for launch.`);
-         } else if (cargoMassInput.value > 10000) {
             event.preventDefault();
+         } else if (cargoMassInput.value > 10000) {
             faultyItems.style.visibility = "visible";
             cargoStatus.innerHTML = `Cargo too Heavy.`;
             launchStatus.style.color = "red";
             launchStatus.innerHTML = `Shuttle not ready for launch.`;
             event.preventDefault();
-         } else {
+          } else {
+            faultyItems.style.visibility = "hidden";
             launchStatus.style.color = "green";
             launchStatus.innerHTML = `Shuttle is ready for launch.`;
+            cargoStatus.innerHTML = `Cargo mass low enough for launch.`;
+            fuelStatus.innerHTML = `Fuel level high enough for launch.`;
             event.preventDefault();
          }
       });    
         
-            // event.preventDefault();
+           
          /*TODO:;
-          else if cargoMassInput.value is more than 10k kg
+          else if cargoMassInputValue is more than 10k kg
             faultyItems = visible, cargoStatus = "cargo too heavy"
             h2LaunchStatus = "shuttle not ready for launch" & color red
          else 
