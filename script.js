89791fbce0b533698ba70f94aa2bbb5cbd867d5d
Ella Vankirk <ella.vankirk@gmail.com>
2020-10-28 23:22:37 -0500

    Completed, 1st full Draft

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   init();
-});
-
-   function init() {
-      let form = document.querySelector("form");
-      let pilotInput = document.querySelector("input[name=pilotName]");
-      let copilotInput = document.querySelector("input[name=copilotName]");
-      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-      let cargoMassInput = document.querySelector("input[name=cargoMass]");
-
-      let fuelLevelInputValue = Number("fuelLevelInput.value");
-      // let cargoMassInputValue = Number("cargoMassInputValue");
-
-      let faultyItems = document.getElementById("faultyItems");
-      let pilotStatus = document.getElementById("pilotStatus");
-      let copilotStatus = document.getElementById("copilotStatus");
-      let fuelStatus = document.getElementById("fuelStatus");
-      let cargoStatus = document.getElementById('cargoStatus');
-      let launchStatus = document.getElementById("launchStatus");
-
-      form.addEventListener("submit", function(event) {
-         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-            response.json().then( function(json){
-               console.log(json);
+   let json = [];
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then( function(json) {
+         function selectRandomDestination (planetArray) {
+            return Math.floor(Math.random()*planetArray.length);
+         }
+         const randomDestination = (selectRandomDestination(json)); 
+         const missionTarget = document.getElementById("missionTarget");
+            missionTarget.innerHTML = `
+                  <h2>Mission Destination</h2>
+                     <ol>
+                        <li>Name: ${json[randomDestination].name}</li>
+                        <li>Diameter: ${json[randomDestination].diameter}</li>
+                        <li>Star: ${json[randomDestination].star}</li>
+                        <li>Distance from Earth: ${json[randomDestination].distance}</li>
+                        <li>Number of Moons: ${json[randomDestination].moons}</li>
+                     </ol>
+                     <img src="${json[randomDestination].image}">
+                     `
             });
          });
+   init();
+});
+/*
+*/
+
+   function init() {
+      const form = document.querySelector("form");
+      const pilotInput = document.querySelector("input[name=pilotName]");
+      const copilotInput = document.querySelector("input[name=copilotName]");
+      const fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      const cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+      // const fuelLevelInputValue = Number("fuelLevelInput.value");
+      // const cargoMassInputValue = Number("cargoMassInputValue");
+
+      const faultyItems = document.getElementById("faultyItems");
+      const pilotStatus = document.getElementById("pilotStatus");
+      const copilotStatus = document.getElementById("copilotStatus");
+      const fuelStatus = document.getElementById("fuelStatus");
+      const cargoStatus = document.getElementById('cargoStatus');
+      const launchStatus = document.getElementById("launchStatus");
+
+      form.addEventListener("submit", function(event) {
          if (pilotInput.value === "" ||  copilotInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value ==="") {
             alert("All fields are required");
                event.preventDefault();
@@ -58,15 +76,7 @@ window.addEventListener("load", function() {
             event.preventDefault();
          }
       });    
-        
-           
-         /*TODO:;
-          else if cargoMassInputValue is more than 10k kg
-            faultyItems = visible, cargoStatus = "cargo too heavy"
-            h2LaunchStatus = "shuttle not ready for launch" & color red
-         else 
-*/
-  
+
 }
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
